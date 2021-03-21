c1747945730fe70649f0126c6accd947d5f694f6
psamarina <69726845+psamarina@users.noreply.github.com>
2021-03-21 20:52:05 +0300

    Update Program.cs

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -6,7 +6,24 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p1.x = 4;
+            p1.y = 5;
+            p1.c = '*';
+            p1.Draw();
+
+            Console.ReadLine();
         }
+
+
     }
 }
