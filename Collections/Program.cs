82d7cbe2bca17184410ab5bb5d720f155cb0af78
psamarina <69726845+psamarina@users.noreply.github.com>
2021-03-21 23:00:48 +0300

    lesson6

---
+++ Collections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Tetris;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[5];
+            nums1[0] = 1;
+            nums1[1] = 2;
+
+            //for(int i = 0; i < nums1.length; i++)
+            //{
+            //    console.writeline(nums1[i]);
+            //}
+
+            //foreach(int i in nums1)
+            //{
+            //    console.writeline(i);
+            //}
+
+            //point[] points = new point[3];
+            //points[0] = new point(1, 2, '*');
+            //points[1] = new point(3, 4, '#');
+            //points[2] = new point(5, 6, '*');
+
+            //foreach(point p in points)
+            //{
+            //    p.draw();
+            //}
+
+            char[][] field = new char[3][];
+            field[0] = new char[3];
+            field[1] = new char[3];
+            field[2] = new char[3];
+
+            field[0][0] = 'X';
+            field[2][2] = '0';
+
+            for(int i = 0; i < field.Length; i++)
+            {
+                for(int j = 0; j < field[i].Length; j++)
+                {
+                    Console.Write(field[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- Tetris/Point.cs
+++ Tetris/Point.cs
@@ -4,18 +4,25 @@ using System.Text;
 
 namespace Tetris
 {
-    class Point
+    public class Point
     {
         public int x;
         public int y;
         public char c;
 
-        public void Draw() 
+        public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
         }
-        public Point(int a, int b, char sym);
+        public Point(int a, int b, char sym)
+        {
+            x = a;
+            y = b;
+            c = sym;
+        }
+
+        public Point() { }
 
     }
 }
